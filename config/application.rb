6e5762cf0dc097807ec754ff8fa7c2f6ea639942
krongk <kenrome@gmail.com>
2010-10-31 23:28:06 +0800

    add session

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module Laopo
     # config.action_controller.perform_caching = true
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,9 @@
 
 Laopo::Application.config.session_store :cookie_store, :key => '_laopo_session'
 
+#Laopo::Application.config.session_store :expire_after => 1.minutes.from_now
+#ActionController::Base.session_options[:session_expires] = 1.minutes.from_now
+
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
